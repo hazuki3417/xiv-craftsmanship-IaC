b5b21e84d6c7a238b20d95fea8acd98f691bf39e
Takashi Kudo <hazuki3417@gmail.com>
2024-04-02 15:21:04 +0900

    chore: iamの作成処理を修正

--- memoria/lib/memoria-record/iam.ts
+++ memoria/lib/memoria-record/iam.ts
@@ -1,4 +1,4 @@
-import * as iam from 'aws-cdk-lib/aws-iam';
+import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import env, { getEnvName } from '../environment';
 
@@ -6,14 +6,40 @@ export class IAMResources extends Construct {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    if (env().deny(['development'])) {
-      throw new Error(`Skip resource generation in ${getEnvName()} environment`);
+    if(env().allow(['production', 'staging'])){
+      // production,stagingのときはコンテナにアタッチするためのIAMロールを作成する
+      // TODO: 第二引数の用途を調べる
+      const role = new cdk.aws_iam.Role(this, 'MemoriaRecordIAM', {
+        assumedBy: new cdk.aws_iam.ServicePrincipal('lambda.amazonaws.com'),
+        roleName: `app.memoria-record-${getEnvName()}`,
+      });
+      // TODO: アクセス範囲を制限する
+      role.addManagedPolicy(cdk.aws_iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonS3FullAccess'));
     }
 
-    // TODO: 第二引数の用途を調べる
-    new iam.Role(this, 'MemoriaRecordIAM', {
-      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
-      roleName: 'memoria-record-role',
-    });
+    if (env().allow(['development'])) {
+      // developmentのときはローカルで実行するためのIAMクレデンシャルを作成する
+      // TODO: 第二引数の用途を調べる
+      const user = new cdk.aws_iam.User(this, 'MemoriaRecordIAM', {
+        userName: `app.memoria-record-${getEnvName()}`,
+      });
+      // TODO: アクセス範囲を制限する
+      user.addManagedPolicy(cdk.aws_iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonS3FullAccess'));
+
+
+      // IAMユーザーのアクセスキーを作成
+      const accessKey = new cdk.aws_iam.CfnAccessKey(this, 'MemoriaRecordIAMUserAccessKey', {
+        userName: user.userName
+      });
+
+      // 出力にアクセスキーとシークレットアクセスキーを表示
+      new cdk.CfnOutput(this, 'AccessKeyId', {
+        value: accessKey.ref
+      });
+      new cdk.CfnOutput(this, 'SecretAccessKey', {
+        value: accessKey.attrSecretAccessKey
+      });
+    }
+
   }
 }
